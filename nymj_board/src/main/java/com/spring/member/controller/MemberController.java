ab3ea3b77a44d6e094a04906d5d70260a42a20b3
nayoung1225 <sjo608@naver.com>
2024-06-20 15:13:29 +0900

    240620 NY jsp수정

--- nymj_board/src/main/java/com/spring/member/controller/MemberController.java
+++ nymj_board/src/main/java/com/spring/member/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.spring.member.controller;
 
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.main.HomeController;
 
 /**
  * @Class Name		: MemberController.java
@@ -12,17 +19,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 
 @Controller
-@RequestMapping( "/member" )
+@RequestMapping("/member")
 public class MemberController {
+	
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/** 
 	 * 기능		: 회원 로그인 기능
 	 * @Date 	: 2024. 06. 19
 	 * @author  : 진민지	 
 	 */
-	@RequestMapping(value= "/login", method = RequestMethod.POST)
-	public String loginView() {
-		return "/views/login.jsp";
+	
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String join(Locale locale, Model model) {
+		logger.info("login!", locale);
+		
+		return "login";
 	}
+	
 	
 }
