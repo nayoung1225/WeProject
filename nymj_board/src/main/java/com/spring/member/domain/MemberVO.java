ce5f68fd1ed548a406a7dcd6d7a37e1547590899
nayoung1225 <sjo608@naver.com>
2024-06-21 10:20:39 +0900

    240621 NY MemberVO,BoardVO commit

--- nymj_board/src/main/java/com/spring/member/domain/MemberVO.java
+++ nymj_board/src/main/java/com/spring/member/domain/MemberVO.java
@@ -1,5 +1,19 @@
 package com.spring.member.domain;
 
+import java.util.Date;
+
 public class MemberVO {
 
+	private int memberSeq;
+	private String memberNm;
+	private String memberId;
+	private String memberPw;
+	private Date memberBirth;
+	private String memberAddr;
+	private String memberPhone;
+	private String memberLevel; //ex) 000 운영자 001 관리자 003 회원
+	private String memberPwHint;
+	private String updtDate;
+	private String regDate;
+	
 }
